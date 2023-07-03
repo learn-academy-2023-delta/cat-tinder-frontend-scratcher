9e5995d3d82191b056b965dc8c56aa2795ca5b91
immikeoa <immikeoa@gmail.com>
2023-07-03 10:07:27 -0400

    adds read and show fuctionality.

--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const CatShow = () => {
-  return (
-    <div>CatShow</div>
-  )
-}
+const CatShow = ({ cats }) => {
+  const { id } = useParams();
 
-export default CatShow
+  // Find the cat with the matching id
+  const cat = cats.find((cat) => cat.id === parseInt(id));
+
+   return (
+    <div className="cat-show">
+      <h2>{cat.name}</h2>
+      <img src={cat.img} alt={cat.name} />
+      <p>Age: {cat.age}</p>
+      <p>Enjoys: {cat.enjoys}</p>
+    </div>
+  );
+};
+
+export default CatShow;
